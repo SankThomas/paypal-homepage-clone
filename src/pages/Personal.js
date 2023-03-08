b735d6ee04fad5eb35290aa10832f701a7a0d378
Thomas Sankara <thomassankara74@gmail.com>
2023-03-08 22:12:36 +0300

    render page components

--- src/pages/Personal.js
+++ src/pages/Personal.js
@@ -1,5 +1,16 @@
 import React from "react";
+import SignUp from "./components/homepage/SignUp";
+import Hero from "./personal/Hero";
+import Section2 from "./personal/Section2";
+import Section3 from "./personal/Section3";
 
 export default function Personal() {
-  return <div>Personal</div>;
+  return (
+    <>
+      <Hero />
+      <Section2 />
+      <Section3 />
+      <SignUp />
+    </>
+  );
 }
