373d284a1033884dc2580b8d4e9d808cdab59a91
Thomas Sankara <thomassankara74@gmail.com>
2023-03-15 16:23:24 +0300

    merchant page

--- src/pages/Merchant.js
+++ src/pages/Merchant.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Hero from "./merchant/Hero";
+import Section2 from "./merchant/Section2";
+import Section3 from "./merchant/Section3";
+import Section4 from "./merchant/Section4";
+import Section5 from "./merchant/Section5";
+import Section6 from "./merchant/Section6";
 
 export default function Merchant() {
-  return <div>Merchant</div>;
+  return (
+    <>
+      <Hero />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+    </>
+  );
 }
