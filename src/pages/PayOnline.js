57a48c3123d2638fc6eaededd96e65d5a5941657
Thomas Sankara <thomassankara74@gmail.com>
2023-03-15 16:23:54 +0300

    pay online page

--- src/pages/PayOnline.js
+++ src/pages/PayOnline.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Hero from "./payonline/Hero";
+import ImageGrids from "./payonline/ImageGrids";
+import Section4 from "./payonline/Section4";
+import Speed from "./payonline/Speed";
+import SignUp from "./components/homepage/SignUp";
 
 export default function PayOnline() {
-  return <div>PayOnline</div>;
+  return (
+    <>
+      <Hero />
+      <Speed />
+      <ImageGrids />
+      <Section4 />
+      <SignUp />
+    </>
+  );
 }
