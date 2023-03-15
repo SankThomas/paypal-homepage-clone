2d7b7ea80661db330186122b3c5e6cb330057eaa
Thomas Sankara <thomassankara74@gmail.com>
2023-03-15 16:23:38 +0300

    mobile apps page

--- src/pages/MobileApps.js
+++ src/pages/MobileApps.js
@@ -1,5 +1,26 @@
 import React from "react";
+import mobileApps from "../images/mobileapps/mobileapps.png";
 
 export default function MobileApps() {
-  return <div>MobileApps</div>;
+  return (
+    <>
+      <section className="mobile-apps mb-40">
+        <div className="max-w-6xl mx-auto grid grid-cols-1 gap-8 lg:grid-cols-2 lg:items-center py-10 lg:pb-0 px-8 text-center lg:text-left">
+          <article className="text-white">
+            <h1 className="text-4xl lg:text-5xl mb-4 font-light">
+              Tap into your money.
+            </h1>
+            <p>
+              Send payments to almost anywhere in the world in seconds. Manage
+              the ways you send and spend at a glance. It’s all here.
+            </p>
+          </article>
+
+          <article>
+            <img src={mobileApps} alt="" className="block mx-auto lg:-mb-40" />
+          </article>
+        </div>
+      </section>
+    </>
+  );
 }
