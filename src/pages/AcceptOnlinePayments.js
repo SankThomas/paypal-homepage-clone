b7a75b788b9e719b0b5442a8a6729e52d4ff474f
Thomas Sankara <thomassankara74@gmail.com>
2023-03-15 16:23:10 +0300

    accept online payments page

--- src/pages/AcceptOnlinePayments.js
+++ src/pages/AcceptOnlinePayments.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Checkout from "./onlinepayments/Checkout";
+import CTA from "./onlinepayments/CTA";
+import Hero from "./onlinepayments/Hero";
+import Section3 from "./onlinepayments/Section3";
+import Section5 from "./onlinepayments/Section5";
+import Tabs from "./onlinepayments/Tabs";
 
 export default function AcceptOnlinePayments() {
-  return <div>AcceptOnlinePayments</div>;
+  return (
+    <>
+      <Hero />
+      <Checkout />
+      <Section3 />
+      <Tabs />
+      <Section5 />
+      <CTA />
+    </>
+  );
 }
