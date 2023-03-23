815d44cbe71909cc49ad53f384b91f9d6d6e067a
Thomas Sankara <thomassankara74@gmail.com>
2023-03-23 14:51:31 +0300

    invoices page

--- src/pages/SendInvoice.js
+++ src/pages/SendInvoice.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Convert from "./invoices/Convert";
+import GetPaid from "./invoices/GetPaid";
+import Hero from "./invoices/Hero";
+import HowItWorks from "./invoices/HowItWorks";
+import Section2 from "./invoices/Section2";
+import SendingInvoices from "./invoices/SendingInvoices";
 
 export default function SendInvoice() {
-  return <div>SendInvoice</div>;
+  return (
+    <>
+      <Hero />
+      <Section2 />
+      <HowItWorks />
+      <SendingInvoices />
+      <Convert />
+      <GetPaid />
+    </>
+  );
 }
