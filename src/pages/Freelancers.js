c8a1336d01945c4d940563fa4c51f6d2dcbd69a6
Thomas Sankara <thomassankara74@gmail.com>
2023-03-23 14:51:20 +0300

    freelancers page

--- src/pages/Freelancers.js
+++ src/pages/Freelancers.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Hero from "./freelancers/Hero";
+import Why from "./freelancers/Why";
+import Section3 from "./components/homepage/Section3";
+import Looking from "./freelancers/Looking";
+import GetStarted from "./freelancers/GetStarted";
 
 export default function Freelancers() {
-  return <div>Freelancers</div>;
+  return (
+    <>
+      <Hero />
+      <Why />
+      <Section3 />
+      <Looking />
+      <GetStarted />
+    </>
+  );
 }
