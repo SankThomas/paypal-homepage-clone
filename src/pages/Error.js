a45cb123dfe46e38030876359ebe8d9b5513929e
Thomas Sankara <thomassankara74@gmail.com>
2023-03-31 18:26:10 +0300

    create error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <>
+      <section>
+        <article
+          style={{
+            backgroundColor: "#333333",
+          }}
+          className="text-white text-center py-20 px-8"
+        >
+          <h1 className="text-4xl lg:text-7xl font-light mb-2">
+            Something's not right.
+          </h1>
+          <p>That page doesn't exist.</p>
+        </article>
+
+        <article className="py-20 text-center px-8">
+          <p className="mb-8">We can’t find a page with the url you entered.</p>
+          <Link to="/" className="text-blue-600 font-bold hover:underline">
+            Back to Civilization
+          </Link>
+        </article>
+      </section>
+    </>
+  );
 }
